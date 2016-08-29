a75cf6afa6ae886e07bdc3c15ced536fd40c443e
polaris610 <polaris.tiger803@yandex.com>
2016-08-29 11:52:24 +0300

    Review Module Changes

---
+++ app/local/modules/Review/bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+class IndividualPush_Bootstrap {
+
+    public static function init($bootstrap)
+    {
+        $base = Core_Model_Directory::getBasePathTo("/app/local/modules/Review/");
+
+        # Models
+        require_once "{$base}/Model/Review.php";
+        require_once "{$base}/Model/Db/Table/Review.php";
+    }
+}
--- app/local/modules/Review/controllers/Mobile/ViewController.php
+++ app/local/modules/Review/controllers/Mobile/ViewController.php
@@ -2,5 +2,13 @@
 
 class Review_Mobile_ViewController extends Application_Controller_Mobile_Default
 {
-			
+	public function findAction()
+	{
+		
+	}
+	
+	public function postAction()
+	{
+		
+	}
 }
